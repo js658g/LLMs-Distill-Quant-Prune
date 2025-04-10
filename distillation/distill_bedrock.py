0c8754645fc41a4346470ee18eea06fdb03e0e05
Jay <surenderjamplu@gmail.com>
2025-04-10 01:08:04 +0000

    torch distilll

--- distillation/distill_bedrock.py
+++ distillation/distill_bedrock.py
@@ -10,7 +10,8 @@ model_name = f"distilled-model-{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
 # Configure your models and IAM role
 teacher_model = "arn:aws:bedrock:us-west-2::foundation-model/meta.llama3-1-70b-instruct-v1:0"
 student_model = "arn:aws:bedrock:us-west-2::foundation-model/meta.llama3-1-8b-instruct-v1:0:128k"
-role_arn = "arn:aws:iam::<YOUR_ACCOUNT_ID>:role/<YOUR_IAM_ROLE>"
+
+#role_arn = "arn:aws:iam::<YOUR_ACCOUNT_ID>:role/<YOUR_IAM_ROLE>"
 # Specify S3 locations for training data and output
 training_data = "s3://<YOUR_BUCKET>/training-data.jsonl" # Replace by your training file
 output_path = "s3://<YOUR_BUCKET>/output/" 
